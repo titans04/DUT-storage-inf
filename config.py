267eb9eeeaca15995f1dc6e31164672fb149163f
Bandile <bandilecele18@gmail.com>
2025-10-13 08:59:51 +0200

    updated the config.py file

--- config.py
+++ config.py
@@ -1,17 +1,11 @@
 import os
 
-# Get the base directory of the project
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    """
-    Set Flask configuration variables from environment variables.
-    """
-    # General Config
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'a-very-secret-key-that-you-should-change')
-    FLASK_APP = os.environ.get('FLASK_APP', 'run.py')
-    FLASK_ENV = os.environ.get('FLASK_ENV', 'development')
-
-    # Database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'instance', 'app.db'))
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'a-very-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        f"sqlite:///{os.path.join(basedir, 'instance', 'app.db')}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
